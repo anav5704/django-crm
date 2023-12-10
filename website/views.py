5a72d7790786c71eb6f81aca9d90ba93b231f1f1
anav5704 <anavkki@gmail.com>
2023-12-10 14:04:51 +1200

    feat: add user auth

--- website/views.py
+++ website/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as authLogin, logout as authLogout
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def home(req):
-    return render(req, "home.html", {})
+    if req.method == "POST":
+        username = req.POST["username"]
+        passwd = req.POST["passwd"]
+
+        user = authenticate(req, username=username, password=passwd)
+
+        if user is not None:
+            authLogin(req, user)
+            messages.success(req, "You've been successfully logged in!")
+            return redirect("home")
+        else:
+            messages.error(req, "Oops, something went wrong!")
+            return redirect("home")
+        
+    else:
+        return render(req, "home.html", {})
+    
+def logout(req):
+    pass
